76f37ab1a578c59ea90c529e2d0988bc47a78f77
Weslley-13 <106767805+Weslley-13@users.noreply.github.com>
2022-06-20 13:30:31 -0300

    Create index.js

---
+++ Js/index.js
@@ -0,0 +1,8 @@
+let nome="Weslley"
+let sobreNome="Arruda"
+let dataDeNascimento=1997
+let cidade="São paulo"
+let anoAtual=2022
+let quemSou='Eu sou Weslley Arruda Rodrigues, tenho 25 anos, nasci na cidade de São paulo onde habito atualmente.'
+console.log(anoAtual - dataDeNascimento) 
+console.log(quemSou)
